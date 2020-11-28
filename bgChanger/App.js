ce968516bd66767547f6c27fc0a200af585c5e9a
Gajendra Mehra <gajendramehra14rgnv@gmail.com>
2020-11-28 16:43:12 +0530

    b changer app completed

--- bgChanger/App.js
+++ bgChanger/App.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
+const chalk = require('chalk');
 import {
   Text,
   StyleSheet,
-  View
+  View,
+  TouchableOpacity
 
 } from 'react-native'
 const App =()=>{
+
+ 
+  const [randomColor,setRandomColor]=useState('rgb(32,0,126)');
+  const changeBG=()=>{
+    console.log(chalk.green('Hello world!'));
+    let color = `rgb(
+      ${  Math.floor(Math.random()*256)},
+      ${  Math.floor(Math.random()*256)},
+      ${  Math.floor(Math.random()*256)}
+      )`
+      setRandomColor(color)
+
+  }
   return (
     <>
     <View style={
     [ styles.container,
-    { backgroundColor:"#123abc"}
+    { backgroundColor:randomColor}
     ]
     }>
-      <Text>First change here </Text>
+      <TouchableOpacity onPress={changeBG}>
+        <Text style={styles.text}>Tap Me</Text>
+
+
+      </TouchableOpacity>
     </View>
     </>
   );
@@ -27,5 +46,14 @@ const styles= StyleSheet.create({
     justifyContent:'center',
     // color:'#ffffff'
    
+  },
+  text:{
+    fontSize:30,
+    backgroundColor:"#BB2cd9",
+    paddingVertical:10,
+    paddingHorizontal:40,
+    color:"#ffffff",
+    borderRadius:15,
+    textTransform:"uppercase"
   }
 })
