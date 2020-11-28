843ecb26c24c2cf1b046ec67b280c84c9cb9ce38
Gajendra Mehra <gajendramehra14rgnv@gmail.com>
2020-11-28 16:13:51 +0530

    Project Bg Changer Setup

---
+++ bgChanger/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+  Text,
+  StyleSheet,
+  View
+
+} from 'react-native'
+const App =()=>{
+  return (
+    <>
+    <Text>First change here </Text>
+    </>
+  );
+}
+export default App;
