46df77c35708cb118233bd7e448031a1b214382f
Gajendra Mehra <gajendramehra14rgnv@gmail.com>
2020-12-05 17:06:40 +0530

    redux functionality completed

---
+++ reduxApp/src/screens/Add.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+    Text
+} from 'native-base'
+function Add() {
+    return (
+        <>
+            <Text>Add</Text>
+        </>
+    )
+}
+
+export default Add
---
+++ reduxApp/src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+    Text
+} from 'native-base'
+function Home() {
+    return (
+        <>
+            <Text>Home</Text>
+        </>
+    )
+}
+
+export default Home
